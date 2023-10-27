b492dc6b18cc2419ea3124d2dde0b18168ee0018
Filip Djuric <filipdjuric.dev@gmail.com>
2023-10-27 18:04:24 +0200

    Added on Scroll Animations

---
+++ public/script/scrollAnimate.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            if(entry.target.classList.contains('faq-row')){
+                console.log("FAQ");
+                setTimeout(function () {
+                    entry.target.classList.add('scrollAnimate');
+                }, 500)
+            }else {
+                entry.target.classList.add('scrollAnimate');
+            }
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElementsMoveUp = document.querySelectorAll('.hiddenBot');
+const hiddenElementsMoveRight = document.querySelectorAll('.hiddenLeft');
+
+hiddenElements.forEach((el) => observer.observe(el));
+hiddenElementsMoveUp.forEach((el) => observer.observe(el));
+hiddenElementsMoveRight.forEach((el) => observer.observe(el));
+
+})
