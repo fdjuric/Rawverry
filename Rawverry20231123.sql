8f5aae99c30de82e1a235d22d9a849fc7b9cc6fd
Filip Djuric <filipdjuric.dev@gmail.com>
2023-11-23 15:55:28 +0100

    changed database file

--- Rawverry20231123.sql
+++ Rawverry20231123.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: auriss
+-- Host: localhost    Database: rawverry
 -- ------------------------------------------------------
 -- Server version	8.0.34
 
@@ -16,94 +16,91 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `accounts`
+-- Table structure for table `account`
 --
 
-DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `accounts` (
+CREATE TABLE `account` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(10) DEFAULT NULL,
-  `user_password` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `date_col` date DEFAULT NULL,
+  `user_name` varchar(20) DEFAULT NULL,
+  `user_password` varchar(300) DEFAULT NULL,
+  `user_email` varchar(100) DEFAULT NULL,
   `account_role` enum('Regular','Editor','Admin') DEFAULT 'Regular',
-  `picture_path` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `picture_path` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `date_col` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `accounts`
+-- Dumping data for table `account`
 --
 
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (3,'primary','$2b$10$YLFHIQ45CR/ms36Dznes7eI3pcz6hi/Je/Ba2pmoYdnkwdlOXAz5a',NULL,NULL,'Regular',NULL),(4,'Admin','$2b$10$zgcj.8afFKdNn//87.nzcut/3ny3.4WO4HLKDGKC7FgLh/nV2UcsO',NULL,NULL,'Regular',NULL);
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (7,'Cupenzi','$2b$10$KtdSgAz3wVn0QXIjbzEyPOYNNbBYatZpFD1AXzjTgXxDPOGi94WS6','filipdjuric.dev@gmail.com','Admin','images/profile/gigachad-chad.gif','n06e5q2Rl2Jyi1mya5NICaKjPJiteLGI9dDQkqZssBFk5ykir4DzpfqOPZLqhb1BSWhl6dZWrKfZV411oBlGDgfOIr13GrQ9zGXsX6ym0sHz0hlvR0Wn95SijZZZIt2K','2023-10-22'),(8,'rawverry','$2b$10$v6zK0N9EfrBNzDOh1JNwFOMHK.2K6sAbvk7jnQCVi.WTWwnqkFUeS','herklias@gmail.com','Admin',NULL,'KRbHLXGL519rYEQZz1PjGBWoY6AA4AWqXZtxVZatyAWGS5yNxioYdiyIkOIlTDqCM1YXRodbPZIpgoG7jQ3EcBWyxQMS1TlkzhWyi86XFYvewI34bVrM2NAj2rwwInzn','2023-10-22'),(9,'testEditor','$2b$10$IE3LFfU5/o31e.RuQvYDke.Llbxq6AXlgS7Hol1brHBMzezq2./oG','filipcupo123@gmail.com','Editor','images/profile/gigachad-chad.gif','N6W2tvkO155XyJG5FAkz8aO9jkCKMCVFfGZgtzLnmalF4Gb45I3w4X6k8LYvC5nGGgnr8B8lVxBRJc3MfWt8xYWeVlts59EWsK9iNF0VsnGRQhOzVLAcGX1DoZEpxZmq','2023-11-17'),(10,'testUser','$2b$10$DOC66YxiS9zsjsVfyBQONu8cpQvbzxZQBYZxOl.moOjdBprEipf1K','sadsamurai333@gmail.com','Regular',NULL,'5J3Ns8S2ITPFOTjEwIbqqTWy8kHYGKrMNogVrFK53Kpi5myECoiA2bEB5bZNebX1g23ThINR6ydePU0cF7jVKeT1y39899SmgDywye0UMpmCQgTNoG5Hio23WXmqJK4G','2023-11-18');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `billing`
+-- Table structure for table `blog`
 --
 
-DROP TABLE IF EXISTS `billing`;
+DROP TABLE IF EXISTS `blog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `billing` (
+CREATE TABLE `blog` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  `surname` varchar(30) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  `postal` int DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  `date_col` date DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `billing`
+-- Dumping data for table `blog`
 --
 
-LOCK TABLES `billing` WRITE;
-/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
-/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+LOCK TABLES `blog` WRITE;
+/*!40000 ALTER TABLE `blog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `coupons`
+-- Table structure for table `coupon`
 --
 
-DROP TABLE IF EXISTS `coupons`;
+DROP TABLE IF EXISTS `coupon`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `coupons` (
+CREATE TABLE `coupon` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `coupon_code` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `discount_type` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `discount_amount` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `coupon_code` varchar(50) DEFAULT NULL,
+  `discount_type` varchar(50) DEFAULT NULL,
+  `discount_amount` varchar(50) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   `expiration_date` date DEFAULT NULL,
   `maximum_users` int DEFAULT NULL,
-  `product_restrictions` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `product_restrictions` varchar(1000) DEFAULT NULL,
   `minimum_order_amount` decimal(10,2) DEFAULT NULL,
-  `redemption_status` enum('Active','Used') DEFAULT 'Active',
+  `redemption_status` enum('Active','Used') DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `coupons`
+-- Dumping data for table `coupon`
 --
 
-LOCK TABLES `coupons` WRITE;
-/*!40000 ALTER TABLE `coupons` DISABLE KEYS */;
-INSERT INTO `coupons` VALUES (2,'SUMMERSALE','percent','10','2023-04-01','2023-04-04',NULL,NULL,NULL,'Active');
-/*!40000 ALTER TABLE `coupons` ENABLE KEYS */;
+LOCK TABLES `coupon` WRITE;
+/*!40000 ALTER TABLE `coupon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coupon` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -115,10 +112,12 @@ DROP TABLE IF EXISTS `newsletter`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `newsletter` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `date_col` date DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `date_col` date NOT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `isConfirmed` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +126,69 @@ CREATE TABLE `newsletter` (
 
 LOCK TABLES `newsletter` WRITE;
 /*!40000 ALTER TABLE `newsletter` DISABLE KEYS */;
+INSERT INTO `newsletter` VALUES (1,'sadsamurai333@gmail.com','2023-09-24','DbmhK7L419kekXEkahUe2ZiKPVJZPshTv9brmU4EpG5f6rimb09lCUu0X2KVyIm6',1),(2,'filipdjuric.dev@gmail.com','2023-09-24','aDQ9o64Aiv9t6caW8ojZToxwqhU2hKgrEXN66W0aoA3JzWsQZBb7kIMuyucuctLv',1),(20,'filipcupo123@gmail.com','2023-09-26','TkVzB1dZBopuf9l8WdLWzStKoSayn8l8LUZzKDFGBYzn4aLDQR1OrRLEzGgo8B2DfzfUCLFAJztNUHIaNeQ4HYadPiCd6fo1gvQ8TvPB3b3NMx9UEDt1sd8raeHBCjCm',1);
 /*!40000 ALTER TABLE `newsletter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `surname` varchar(30) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `postal` int DEFAULT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `date_col` date DEFAULT NULL,
+  `status` enum('Pending','Success','Refunded') DEFAULT NULL,
+  `items` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(50) DEFAULT NULL,
+  `product_price` decimal(10,2) DEFAULT NULL,
+  `product_amount_bought_total` bigint unsigned DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `size` varchar(50) DEFAULT NULL,
+  `date_col` date DEFAULT NULL,
+  `product_price_reduced` decimal(10,2) DEFAULT NULL,
+  `in_stock` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -139,9 +200,9 @@ DROP TABLE IF EXISTS `product_category`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product_category` (
   `category_id` int NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `category_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,7 +211,7 @@ CREATE TABLE `product_category` (
 
 LOCK TABLES `product_category` WRITE;
 /*!40000 ALTER TABLE `product_category` DISABLE KEYS */;
-INSERT INTO `product_category` VALUES (1,'catalog'),(2,'bestsellers');
+INSERT INTO `product_category` VALUES (1,'Abstract Art'),(2,'Figure Drawing'),(3,'Favourites');
 /*!40000 ALTER TABLE `product_category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,14 +223,12 @@ DROP TABLE IF EXISTS `product_category_link`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product_category_link` (
-  `product_id` int NOT NULL,
-  `category_id` int NOT NULL,
-  PRIMARY KEY (`product_id`,`category_id`),
-  KEY `fk_category_id` (`category_id`),
-  CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`),
-  CONSTRAINT `product_category_link_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
-  CONSTRAINT `product_category_link_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`),
-  CONSTRAINT `product_category_link_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`)
+  `product_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  KEY `product_id` (`product_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `product_category_link_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`),
+  CONSTRAINT `product_category_link_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -179,7 +238,6 @@ CREATE TABLE `product_category_link` (
 
 LOCK TABLES `product_category_link` WRITE;
 /*!40000 ALTER TABLE `product_category_link` DISABLE KEYS */;
-INSERT INTO `product_category_link` VALUES (0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1),(11,1),(12,1),(13,1),(14,1),(15,1),(0,2),(1,2),(2,2),(3,2),(4,2),(5,2);
 /*!40000 ALTER TABLE `product_category_link` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,12 +251,15 @@ DROP TABLE IF EXISTS `product_images`;
 CREATE TABLE `product_images` (
   `id` int NOT NULL AUTO_INCREMENT,
   `product_id` int DEFAULT NULL,
-  `path` varchar(255) DEFAULT NULL,
+  `product_price` decimal(10,2) DEFAULT NULL,
+  `product_amount_bought_total` bigint unsigned DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `size` varchar(50) DEFAULT NULL,
+  `date_col` date DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_product_id` (`product_id`),
-  CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
-  CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -207,110 +268,36 @@ CREATE TABLE `product_images` (
 
 LOCK TABLES `product_images` WRITE;
 /*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
-INSERT INTO `product_images` VALUES (1,0,'images/highergoods.webp'),(2,1,'images/pastelromance.webp'),(3,2,'images/infinitelight.webp'),(4,3,'images/ancientsea.webp'),(5,4,'images/bluemist.webp'),(6,5,'images/shiftingspaces.webp'),(7,6,'images/merger.webp'),(8,7,'images/reflection.webp'),(9,8,'images/concretemountains.webp'),(10,9,'images/stolendarkness.webp'),(11,10,'images/meltedgold.webp'),(12,11,'images/doublesensation.webp'),(13,12,'images/goldenfall.webp'),(14,13,'images/diversity.webp'),(15,14,'images/greekvillage.webp'),(16,15,'images/underwatermysteries.webp'),(17,0,'images/higher1.webp');
 /*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `products`
+-- Table structure for table `transaction`
 --
 
-DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS `transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `products` (
-  `product_id` int NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(50) DEFAULT NULL,
-  `product_price` decimal(10,2) DEFAULT NULL,
-  `product_amount_bought_total` bigint unsigned DEFAULT '0',
-  `description` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `size` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `date_col` date DEFAULT NULL,
-  PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (0,'HIGHER GOODS',549.00,0,NULL,'','2023-01-28'),(1,'PASTEL ROMANCE',499.00,0,NULL,NULL,'2023-01-27'),(2,'INFINITE LIGHT',549.00,0,NULL,NULL,'2023-01-22'),(3,'ANCIENT SEA',549.00,0,NULL,NULL,'2023-01-08'),(4,'BLUE MIST',549.00,0,NULL,NULL,'2023-01-28'),(5,'SHIFTING SPACES',499.00,0,NULL,NULL,'2023-03-16'),(6,'MERGER',449.00,0,NULL,NULL,'2023-01-16'),(7,'REFLECTION',599.00,0,NULL,NULL,'2023-01-28'),(8,'CONCRETE MOUNTAINS',549.00,0,NULL,NULL,'2023-03-03'),(9,'STOLEN DARKNESS',449.00,0,NULL,NULL,'2023-02-26'),(10,'MELTED GOLD',499.00,0,NULL,NULL,'2023-03-09'),(11,'DOUBLE SENSATION',449.00,0,NULL,NULL,'2023-02-11'),(12,'GOLDEN FALL',449.00,0,NULL,NULL,'2023-04-04'),(13,'DIVERSITY',499.00,0,NULL,NULL,'2023-02-03'),(14,'GREEK VILLAGE',449.00,0,NULL,NULL,'2023-03-21'),(15,'UNDERWATER MYSTERIES',499.00,0,NULL,NULL,'2023-03-29');
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `shipping`
---
-
-DROP TABLE IF EXISTS `shipping`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `shipping` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  `surname` varchar(30) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  `postal` int DEFAULT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  `date_col` date DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_shipping_address` (`address`),
-  KEY `idx_shipping_country` (`country`),
-  KEY `idx_shipping_postal` (`postal`),
-  KEY `idx_shipping_phone` (`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shipping`
---
-
-LOCK TABLES `shipping` WRITE;
-/*!40000 ALTER TABLE `shipping` DISABLE KEYS */;
-/*!40000 ALTER TABLE `shipping` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `transactions`
---
-
-DROP TABLE IF EXISTS `transactions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `transactions` (
+CREATE TABLE `transaction` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(50) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
   `transaction_id` int DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  `postal` int DEFAULT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  `items` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `total_spent` int DEFAULT NULL,
-  `status` enum('Pending','Refunded','Success') DEFAULT 'Pending',
+  `items` varchar(1000) DEFAULT NULL,
+  `total_spent` decimal(10,2) DEFAULT NULL,
+  `status` enum('Success','Refunded') DEFAULT NULL,
   `date_col` date DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `address` (`address`),
-  KEY `country` (`country`),
-  KEY `postal` (`postal`),
-  KEY `phone` (`phone`),
-  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`address`) REFERENCES `shipping` (`address`),
-  CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`country`) REFERENCES `shipping` (`country`),
-  CONSTRAINT `transactions_ibfk_3` FOREIGN KEY (`postal`) REFERENCES `shipping` (`postal`),
-  CONSTRAINT `transactions_ibfk_4` FOREIGN KEY (`phone`) REFERENCES `shipping` (`phone`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `transactions`
+-- Dumping data for table `transaction`
 --
 
-LOCK TABLES `transactions` WRITE;
-/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -322,4 +309,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-23 15:47:23
+-- Dump completed on 2023-11-23 15:55:02
